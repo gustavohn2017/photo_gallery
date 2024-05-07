e6b656a2085bba24b4bf6c66f86cc102a7aeb02c
Gustavo Henrique Nascimento <45496473+gustavohn2017@users.noreply.github.com>
2024-05-07 19:25:30 -0300

    new commit

--- main.js
+++ main.js
@@ -16,6 +16,19 @@
 
     $('form').on('submit', function(e){
         e.preventdefault();
-    })
-
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style ="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+               <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real" 
+                  Ver imagem em tamanho real
+               </a>
+            </div> 
+         `).appendTo(novoItem);
+         $(novoItem).appendTo('ul')
+         $(novoItem).fadeIn(5000);
+         $('#endereco-imagem-nova').val('')
+      })
+      // trabalhando com input
  })
